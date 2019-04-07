a1515c201e60d8a32e1d00300c127ad658b43d2e
Katie Moosman <moosman.katie@gmail.com>
2019-04-07 21:14:44 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,7 +5,7 @@ class Song < ActiveRecord::Base
   has_many :notes
 
   def artist_name=(name)
-    self.artist = Artist.find_or_create_by(name: name).id
+    self.artist = Artist.find_or_create_by(name: name)
   end
 
   def artist_name
