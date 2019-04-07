b3163954e352a333b480581eec3eccc4cdc3dda0
Katie Moosman <moosman.katie@gmail.com>
2019-04-07 21:01:55 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -21,7 +21,14 @@ class Song < ActiveRecord::Base
     self.genre ? self.genre.name : nil
   end
 
-  
+  def note_contents
+    notes.collect {|note| note.content}
+  end
+
+  def note_contents=(notes)
+    notes.each do |content|
+      note = self.notes.build(content: content) unless content.empty?
+    end
 
 
 end
